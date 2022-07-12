0b02463469895bbbbd2e7a695d27907276c3ff65
Kevin Beck <kevin@multiplestates.co.uk>
2022-07-12 15:46:03 +0100

    Small styling to chain.

--- components/matter.js
+++ components/matter.js
@@ -49,7 +49,7 @@ export default function MatterExample() {
 
 		// add bodies
 		var group = Body.nextGroup(true);
-		let links = widthScreen < 800 ? 60 : 110;
+		let links = widthScreen < 800 ? 70 : 110;
 		let positionStart = widthScreen < 800 ? widthScreen / 2.5 : widthScreen / 3;
 		let positionEnd =
 			widthScreen < 800 ? widthScreen / 1.25 : widthScreen / 1.5;
@@ -60,15 +60,15 @@ export default function MatterExample() {
 					render: {
 						fillStyle: "transparent",
 						lineWidth: 2,
-						strokeStyle: "white",
+						strokeStyle: "#A4A3A0",
 					},
 				});
 			if (index == links / 2)
 				return Bodies.circle(widthScreen / 3, 0, 5, {
 					render: {
-						fillStyle: "white",
+						fillStyle: "#A4A3A0",
 						lineWidth: 5,
-						strokeStyle: "white",
+						strokeStyle: "#A4A3A0",
 					},
 					density: 0.025,
 				});
@@ -76,7 +76,7 @@ export default function MatterExample() {
 				render: {
 					fillStyle: "transparent",
 					lineWidth: 2,
-					strokeStyle: "white",
+					strokeStyle: "#A4A3A0",
 				},
 			});
 		});
@@ -84,7 +84,7 @@ export default function MatterExample() {
 		Composites.chain(ropeA, 0.5, 0, -0.5, 0, {
 			stiffness: 1,
 			length: 1,
-			render: { type: "line" },
+			render: { type: "line", strokeStyle: "#A4A3A0" },
 			chamfer: 5,
 			// density: 0.05,
 		});
@@ -143,7 +143,7 @@ export default function MatterExample() {
 			if (previousTimeStamp !== timestamp + 10) {
 				element.style.transform =
 					"translate(" +
-					(ropeA.bodies[links / 2].position.x - 60) +
+					(ropeA.bodies[links / 2].position.x - 64) +
 					"px, " +
 					ropeA.bodies[links / 2].position.y +
 					"px)";
@@ -184,12 +184,12 @@ export default function MatterExample() {
 				viewBox="0 0 67 76"
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
-				className="absolute w-20 origin-top md:w-32"
+				className="absolute w-16 origin-top md:w-32"
 				ref={logo}
 			>
 				<path
 					d="M0.384 30.848C7.488 12.512 22.08 0.799995 38.016 0.799995C53.952 0.799995 66.72 12.416 66.72 26.912C66.72 45.92 51.744 60.704 32.352 60.992C26.592 61.088 20.544 51.968 20.544 43.136C20.544 35.264 23.904 29.984 28.512 29.504C33.984 28.928 38.496 39.392 43.584 39.392C45.12 39.392 45.888 37.28 45.6 33.92C44.928 26.144 40.8 20.768 33.696 20.768C23.04 20.768 13.44 30.656 13.44 41.696C13.44 59.36 24.384 68 37.536 68.384C46.752 68.672 56.256 63.392 63.744 54.944L64.512 55.424C56.256 68.096 44.448 75.776 32.64 75.776C18.72 75.776 5.856 66.56 5.376 48.416C4.8 29.12 21.792 17.216 35.808 17.216C45.408 17.216 53.568 22.976 53.568 35.168C53.568 39.296 51.072 45.92 48 45.632C43.488 45.248 37.44 39.296 30.048 38.72C26.208 38.432 23.808 40.832 23.808 43.424C23.808 49.568 33.312 55.232 42.72 54.464C52.704 53.6 60.096 44.48 60.096 32.96C60.096 18.944 49.632 8.48 35.808 8.48C22.464 8.48 9.12 17.216 1.248 31.232L0.384 30.848Z"
-					fill="white"
+					fill="#A4A3A0"
 					strokeWidth="0"
 				/>
 			</svg>
