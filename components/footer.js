5e21f1f7e5b50650d39797b701d508dae7f78dca
Kevin Beck <kevin@multiplestates.co.uk>
2022-07-21 14:40:35 +0100

    Stlye updates to test on devices.

--- components/footer.js
+++ components/footer.js
@@ -5,12 +5,10 @@ export default function Footer() {
   const router = useRouter();
   return (
     <footer className={`fixed bottom-0 z-30 w-full`}>
-      <div className="grid w-full grid-cols-1 p-10 md:grid-cols-2 md:p-16">
+      <div className="grid w-full grid-cols-1 p-8 md:grid-cols-2 md:p-16">
         <div className="">
           <p className="pb-5 text-center text-sm text-white md:pb-0 md:text-left md:text-base">
-            324 Grand St
-            <br />
-            Storefront A<br />
+            324 Grand St, Storefront A<br />
             New York, NY 10002
           </p>
         </div>
